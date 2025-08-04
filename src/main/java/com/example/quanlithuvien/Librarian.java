574c25d661fb1bdfac2e2d42c89cecc0935677d7
honganh116 <hae112k6@gmail.com>
2025-08-04 14:45:28 +0700

    Add files via upload

---
+++ src/main/java/com/example/quanlithuvien/Librarian.java
@@ -0,0 +1,37 @@
+public class Librarian() extends Reader {
+    public addBook(String id, String name, String author) {
+        super(id, name, author);
+    }
+
+    public void RemoveBook(String bookId) {
+
+    }
+
+    public void addUser(String id, String name, String password) {
+
+    }
+
+    public void updateBookName(String bookId, String name) {
+
+    }
+
+    public void updateBookAuthor(String bookId, String name) {
+
+    }
+
+    public addBookTag(String bookId, String tag) {
+
+    }
+
+    public removeBookTag(String bookId, String tag) {
+
+    }
+
+    public updateBookTag(String bookId, String oldTag, String newTag) {
+
+    }
+
+    public findUser(String id) {
+
+    }
+}
