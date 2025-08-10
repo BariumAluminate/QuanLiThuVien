91640335f31884221e835ccc6b66e2bf8dbdb757
Nguyen Huy Hoang <hoangdilamviec@gmail.com>
2025-08-10 17:17:33 +0700

    refactor(Reader): Update variables and add constructor in Reader
    Add variable stringId and rename variables for clarity: csrfToken->csrftoken.
    Add constructor with JavaDoc for initializing Reader.

--- src/main/java/com/example/quanlithuvien/Reader.java
+++ src/main/java/com/example/quanlithuvien/Reader.java
@@ -2,9 +2,23 @@ package com.example.quanlithuvien;
 
 public class Reader {
     private String stringId;
-    protected String password;
     protected String api_KEY;
-    protected String csrfToken;
+    protected String csrftoken;
+    protected String password;
+
+    /**
+     * Phương thức khởi tạo.
+     * @param stringId Id của người đọc
+     * @param password Mật khẩu
+     * @param csrftoken csrftoken
+     * @param api_KEY api_KEY
+     */
+    public Reader(String stringId, String password, String csrftoken, String api_KEY) {
+        this.stringId = stringId;
+        this.password = password;
+        this.api_KEY = api_KEY;
+        this.csrftoken = csrftoken;
+    }
 
     public String getStringId() {
         return stringId;
