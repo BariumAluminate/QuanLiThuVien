48fd1c2f8946545f7c3a2efbb868c79a9b79bfe9
Nguyễn Minh Đức <154437369+BariumAluminate@users.noreply.github.com>
2025-08-13 06:01:34 +0700

    feat(User Interface): implement remove query windows

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -14,6 +14,6 @@ module com.example.UI {
     requires java.net.http;
     requires java.sql;
 
-    opens com.example.quanlithuvien to javafx.fxml, gson;
-    exports com.example.quanlithuvien;
+    opens com.example.UI to javafx.fxml, gson;
+    exports com.example.UI;
 }
