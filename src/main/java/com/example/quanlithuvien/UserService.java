4c17f2ea39292c2be81ee7a7414d4acca503bdb6
Nguyen Huy Hoang <hoangdilamviec@gmail.com>
2025-08-13 21:42:32 +0700

    refactor(UserService): update methods in UserService for better code structure

--- src/main/java/com/example/quanlithuvien/UserService.java
+++ src/main/java/com/example/quanlithuvien/UserService.java
@@ -1,12 +1,19 @@
 package com.example.quanlithuvien;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonSerializer;
 
+import java.io.IOException;
 import java.net.URLEncoder;
+import java.net.http.HttpResponse;
 import java.nio.charset.StandardCharsets;
+import java.util.Objects;
+
+import static com.example.quanlithuvien.Librarian.BASE_URL;
 
 public class UserService {
     /**
@@ -36,7 +43,8 @@ public class UserService {
      * @param api_KEY   api_KEY
      * @param reader    Người dùng được thêm
      */
-    public void addReader(String csrfToken, String stringId, String api_KEY, Reader reader) {
+    public String addReader(String csrfToken, String stringId, String api_KEY, Reader reader)
+            throws IOException, InterruptedException {
         //Dùng URLEncoder.encode để mã hóa các giá trị để tránh lỗi cú pháp URL
         //Ví dụ: Kí tự "=" có thể bị mã hóa thành "%3D"
         //StandardCharsets.UTF_8 là đối tượng Charset được định nghĩa sẵn, tương ứng với mã hóa UTF-8
@@ -45,21 +53,14 @@ public class UserService {
                 "&stringId=" + URLEncoder.encode(stringId, StandardCharsets.UTF_8) +
                 "&API_KEY=" + URLEncoder.encode(api_KEY, StandardCharsets.UTF_8);
 
-        JsonSerializer<Reader> readerJsonSerializer = (reader1, type, jsonSerializationContext) -> {
-            JsonObject jsonObject = new JsonObject();
-            jsonObject.addProperty("stringId", reader1.getStringId());
-            jsonObject.addProperty("name", reader1.getName());
-            jsonObject.addProperty("password", reader1.getPassword());
-            return jsonObject;
-        };
-
         Gson gson = new GsonBuilder()
                 .registerTypeAdapter(Reader.class, readerJsonSerializer)
                 .create();
         String json = gson.toJson(reader);
 
         BookService bookService = new BookService();
-        bookService.doPostRequest(url, json);
+        HttpResponse<String> response = bookService.doPostRequest(url, json);
+        return response.body();
     }
 
     /**
@@ -67,22 +68,15 @@ public class UserService {
      *
      * @param reader Người dùng đăng nhập
      */
-    public void login(Reader reader) {
-        JsonSerializer<Reader> readerJsonSerializer = (reader1, type, jsonSerializationContext) -> {
-            JsonObject jsonObject = new JsonObject();
-            jsonObject.addProperty("stringId", reader1.getStringId());
-            jsonObject.addProperty("name", reader1.getName());
-            jsonObject.addProperty("password", reader1.getPassword());
-            return jsonObject;
-        };
-
+    public String login(Reader reader) throws IOException, InterruptedException {
         Gson gson = new GsonBuilder()
                 .registerTypeAdapter(Reader.class, readerJsonSerializer)
                 .create();
         String json = gson.toJson(reader);
 
         BookService bookService = new BookService();
-        bookService.doPostRequest("http://20.196.64.166:8080/login/authenticate", json);
+        HttpResponse<String> response = bookService.doPostRequest(BASE_URL + "/login/authenticate", json);
+        return response.body();
     }
 
     /**
@@ -90,7 +84,7 @@ public class UserService {
      *
      * @param reader Người thực hiện yêu cầu
      */
-    public void showReaderInfo(Reader reader) {
+    public String showReaderInfo(Reader reader) throws IOException, InterruptedException {
         JsonSerializer<Reader> readerJsonSerializer = (src, typeOfSrc, context) -> {
             JsonObject jsonObject = new JsonObject();
             jsonObject.addProperty("stringId", src.getStringId());
@@ -105,6 +99,7 @@ public class UserService {
         String json = gson.toJson(reader);
 
         BookService bookService = new BookService();
-        bookService.doPostRequest("http://20.196.64.166:8080/reader/show", json);
+        HttpResponse<String> response = bookService.doPostRequest(BASE_URL + "/reader/show", json);
+        return response.body();
     }
 }
