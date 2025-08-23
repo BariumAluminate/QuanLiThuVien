37942971b72131908ddd1075933e8fa4c840ca73
Nguyễn Minh Đức <154437369+BariumAluminate@users.noreply.github.com>
2025-08-23 21:24:01 +0700

    refactor(UI): remove search user function

--- src/main/java/com/example/UI/SearchUser.java
+++
@@ -1,8 +0,0 @@
-package com.example.UI;
-
-public class SearchUser extends SingleLineQuery{
-    public SearchUser() {
-        super("Find an user");
-        Element.setPromptText("Enter the user's ID");
-    }
-}
